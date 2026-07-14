4cb096737f319bcb5f5f99a402295f9b278833cc
juan fernando calle sanchez <127953168+JuanferCalle@users.noreply.github.com>
2026-07-14 15:01:00 -0500

    agregada configuración eslint

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import js from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import parser from "@typescript-eslint/parser";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser,
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {},
+  },
+];
